50a032ee2c3ec1a9f053f39642fb683c4fc6ab2b
moondriver <smoondriver@gmail.com>
2018-12-04 21:04:28 +0000

    [scene] model weight loads okay but cannot remove last layer

--- codes/scene_model.py
+++ codes/scene_model.py
@@ -1,14 +1,15 @@
 # Reference: this code is from [VGG-19 pre-trained model for Keras]
 # https://gist.github.com/baraldilorenzo/8d096f48a1be4a2d660d
 
+import keras
 from keras.models import Sequential
 from keras.layers.core import Flatten, Dense, Dropout
 from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
 from keras.optimizers import SGD
-import cv2, numpy as np
+import cv2, numpy 
 from keras.applications import vgg19
 
-import os, commons, baseline
+import os, commons, baseline, random
 from datetime import datetime
 
 # THIS IS JUST FOR TESTING
@@ -21,7 +22,7 @@ TEST_Y = "test_y.pickle"
 TEST_WHO = "test_who.txt"
 SCENE_MODEL = SCENE_DIR + 'model.joblib'
 
-VGG19_ILSVRC_WEIGHT_PATH = "vgg19_weights.h5"
+VGG19_ILSVRC_WEIGHT_PATH = "./weights/VGG19_ILSVRC_keras_weights.h5"
 
 def VGG_19(weights_path=VGG19_ILSVRC_WEIGHT_PATH):
     model = Sequential()
@@ -104,25 +105,27 @@ def generate_vggface_data():
     with open(commons.TRAIN_FILE, 'r') as f:
         for line in f.readlines():
             train_ids.add(line.strip())
+    train_ids = set(random.sample(train_ids, 80))
 
     test_ids = set([])
     with open(commons.TEST_FILE, 'r') as f:
         for line in f.readlines():
             test_ids.add(line.strip())
+    test_ids = set(random.sample(test_ids, 20))
 
     # encoding and separate (data, label)
     train_x, train_y, train_who, test_x, test_y, test_who = [], [], [], [], [], []
     labelof = commons.get_label()
 
     IMAGE_SIZE = 224
-    cnn_model = VGG_19()
+    vgg_model = VGG_19()
     
     # cnn_model = vgg19.VGG19(weights=VGG19_ILSVRC_WEIGHT_PATH, include_top=False)
 
-    # cnn_model = vgg19.VGG19(weights=VGG19_ILSVRC_WEIGHT_PATH, input_shape=(IMAGE_SIZE, IMAGE_SIZE, 3))
-    # cnn_model.layers.pop(); cnn_model.layers.pop(); cnn_model.layers.pop();
-    # cnn_model.outputs = [cnn_model.layers[-1].output]
-    # cnn_model.layers[-1].outbound_nodes = []
+    # vgg_model = vgg19.VGG19(weights=VGG19_ILSVRC_WEIGHT_PATH, input_shape=(IMAGE_SIZE, IMAGE_SIZE, 3))
+    print(vgg_model.summary())
+    cnn_model = keras.models.Model(inputs=vgg_model.input, outputs=vgg_model.get_layer('fc2').output)
+    print(cnn_model.summary())
 
     print("Loading scene data...", datetime.now())
 
@@ -138,15 +141,25 @@ def generate_vggface_data():
         if imdb_id not in labelof:
             continue
 
+        if imdb_id not in train_ids and imdb_id not in test_ids:
+            continue
+
         image = keras.preprocessing.image.load_img(
             os.path.join(commons.TRAIN_IMAGES_DIR, image_filename), 
             target_size=(IMAGE_SIZE, IMAGE_SIZE))
 
         x = keras.preprocessing.image.img_to_array(image)
-        x = numpy.expand_dims(x, axis=0)
-        x = vgg19.preprocess_input(x, version=1)
-        x = cnn_model.predict(x)
+        
+        # VGG19 requires input to be zero-centered by mean pixel
+        x[:,:,0] -= 103.939
+        x[:,:,1] -= 116.779
+        x[:,:,2] -= 123.68
 
+        x = numpy.expand_dims(x, axis=0)
+        x = vgg19.preprocess_input(x)
+        x = cnn_model.predict(x)
+        
+        print("shape x:", x.shape)
         y = labelof[imdb_id][character_id]
 
         if imdb_id in train_ids:
